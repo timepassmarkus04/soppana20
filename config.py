de11ab4e890e369f086a89756ef72568906ce97f
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-05-07 19:39:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "6009759960:AAEMOCsUrfzdqesHaXE214ERgqQr1sRzlhU")
+TOKEN = getenv("TOKEN", "6701285365:AAEKn6vbsY6D9uFYqYzoE7BO-Wefj-qjtQM")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
