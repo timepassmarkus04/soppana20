fe1582752c6591b7a64115e799f38c31cc4583e3
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-08-02 01:12:38 +0530

    Update Start.py

--- AsuX/modules/Start.py
+++ AsuX/modules/Start.py
@@ -15,7 +15,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 url=f"http://t.me/{context.bot.username}?startgroup=true"),
             InlineKeyboardButton(
                 text="𝑴𝒂𝒊𝒏𝒕𝒂𝒊𝒏𝒆𝒅 𝑩𝒚",
-                url=f"https://t.me/@Daily_grieff"),
+                url=f"https://t.me/Daily_grieff"),
             
         ]
     )
