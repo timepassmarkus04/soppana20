717ad05bf98d5056e3a628f4117d3811597b41c1
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-08-25 19:44:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "6701285365:AAGpb6I2yCCZQkuy8lZ4VmDtMpUIX5-6oPg")
+TOKEN = getenv("TOKEN", "7204172210:AAFk3cTpI2V0HQhiYCzQ2ScM3HE4exL4u5c")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://devilkingp0404:devilkingp0404@cluster0.iw04afs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 )
